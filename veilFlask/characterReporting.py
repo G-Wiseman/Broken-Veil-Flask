025513c0fd3895670ada83170413ca2eac65346d
George Wiseman <georgeawiseman@gmail.com>
2023-04-28 12:27:57 -0700

    Another GET endpoint Done for characters.

--- veilFlask/characterReporting.py
+++ veilFlask/characterReporting.py
@@ -13,11 +13,11 @@ bp = Blueprint('gameinfo', __name__, url_prefix='/gameinfo')
 @bp.route("/parties", methods=["GET"])
 def getParties():
     db = get_db()
-    query = "Select * from Parties where partyId < 3"
+    query = "Select * from Parties"
     return queryDbJson(db, query)
     # Returns a list of all parties the authenticated user has access to. This endpoint should only return parties that the user is authorized to access.
 
-# TODO: This function needs work. 
+# TODO: This function isn't done yet.  
 @bp.route("/parties", methods=["Post"])
 def createNewParty():
     # Creates a new party owned by the authenticated user. The request body should include the name and any other relevant information about the party.
@@ -29,17 +29,23 @@ def getPartybyId(partyID):
     query = "Select * from Parties where partyId= ?;"
     return queryDbJson(db, query, (partyID,))
 
+#TODO: This function isn't done yet.
 @bp.route("/parties/<partyID>", methods=['PUT'])
 def updatePartybyID(partyID):
     return f"update the party withe ID: {partyID}"
 
+
+# Todo: Finish function
 @bp.route("/parties/<partyID>", methods=['DELETE'])
 def deletePartybyID(partyID):
     return f"Delete the party with ID: {partyID}"
 
 @bp.route("/parties/<partyID>/characters", methods=["GET"])
 def getCharactersbyParty(partyID):
-    return f"A JSON with ALL characters that belong to the party ID: {partyID}"
+    db = get_db()
+    query = "select characters.* from parties join characters using (partyId) where partyId = ?;"
+    return queryDbJson(db, query, (partyID,))
+     
 
 @bp.route("/parties/<partyID>/characters", methods=["POST"])
 def updateCharactersbyParty(partyID):
@@ -47,6 +53,9 @@ def updateCharactersbyParty(partyID):
 
 @bp.route("parties/<partyID>/characters/<characterID>", methods=["GET"])
 def getCharacterById(partyID, characterID):
+    db = get_db()
+    query = ""
+    return queryDbJson(db, query, (partyID, characterID))
     return f"Get the character info related to character{characterID} from {partyID}"
 
 @bp.route("parties/<partyID>/characters/<characterID>", methods=["POST"])
